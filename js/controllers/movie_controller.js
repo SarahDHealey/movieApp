63219b0c6de5a4d3128968244847ae2d355c719f
Sarah Healey <s.deane.healey@gmail.com>
2017-05-18 22:50:47 -0600

    deleted comments and consoles

--- js/controllers/movie_controller.js
+++ js/controllers/movie_controller.js
@@ -1,45 +1,7 @@
 
 app.controller('MovieController', function($scope, $http, MoviesFactory, $location, $routeParams) {
   $scope.vw = {};
-  console.log($routeParams.id);
   MoviesFactory.getMovieById($routeParams.id).then(function(response) {
-    console.log('**********response data');
-    console.log(response.data);
     $scope.movie = response.data;
   });
 });
-
-///******question? pass the parameter in the initial funct in a second func?
-
-
-// TO DO
-
-// add a /movies/:id route √
-// render partials/movie.html √
-// it uses a MovieController √
-// Add a function to Factory that makes api call to get one movie by id √
-// set return API value to a scope variable in controller √
-// show
-
-//**************************************************
-//This is the function to getMovie by search term
-//**************************************************
-//     $scope.show = function() {
-//       Movies.getMovie($scope.search)
-//         .then(function(response) {
-//             $scope.movie = response.data;
-//             console.log(response.data);
-//         });
-//     }
-// });
-//**************************************************
-//
-//**************************************************
-
-
-// app.controller('secondController', function($scope, $http, Movies) {
-//     $scope.vw = {};
-//     $scope.vw.search = "Sherlock Holmes";
-//     $scope.vw.searchTerm = Movies.searchTerm;
-//     console.log(Movies.searchTerm);
-// });
--- js/controllers/movies_controller.js
+++ js/controllers/movies_controller.js
@@ -4,38 +4,6 @@ app.controller('MoviesController', function($scope, $http, MoviesFactory, $locat
   $scope.vw.searchTerm = MoviesFactory.searchTerm;
   MoviesFactory.getMovies($scope.vw.searchTerm).then(function(response) {
     $scope.movies = response.data.Search;
-    console.log(response.data.Search);
   });
 });
 
-// TO DO
-
-// add a /movies/:id route √
-// render partials/movie.html √
-// it uses a ShowMovieController √
-// Add a function to Factory that makes api call to get one movie by id
-// set return API value to a scope variable in controller
-// show
-
-//**************************************************
-//This is the function to getMovie by search term
-//**************************************************
-//     $scope.show = function() {
-//       Movies.getMovie($scope.search)
-//         .then(function(response) {
-//             $scope.movie = response.data;
-//             console.log(response.data);
-//         });
-//     }
-// });
-//**************************************************
-//
-//**************************************************
-
-
-// app.controller('secondController', function($scope, $http, Movies) {
-//     $scope.vw = {};
-//     $scope.vw.search = "Sherlock Holmes";
-//     $scope.vw.searchTerm = Movies.searchTerm;
-//     console.log(Movies.searchTerm);
-// });
